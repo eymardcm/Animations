44e347d6d4fe9eac686c013b5a049bf8865cf9c3
Chad Eymard <eymardcm@gmail.com>
2024-02-04 15:20:36 -0500

    Initial Commit

---
+++ Animations/AnimationsApp.swift
@@ -0,0 +1,17 @@
+//
+//  AnimationsApp.swift
+//  Animations
+//
+//  Created by Chad Eymard on 2/4/24.
+//
+
+import SwiftUI
+
+@main
+struct AnimationsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
